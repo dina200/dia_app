64e0925674612b857a0f9656eec91937c35df332
dar'ya kolpakova <dinapeace@ya.ru>
2021-02-15 17:34:58 +0200

    HomePage : layout is done, localization is added

--- lib/src/app/pages/home_page.dart
+++ lib/src/app/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/dia_localizations.dart';
 
 import '../routes.dart' as routes;
+
+part 'widgets/_blood_sugar_storage_form.dart';
 
 class HomePage extends StatelessWidget {
   static const nameRoute = routes.home;
@@ -18,11 +21,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
+    final locale = DiaLocalizations.of(context);
+
+    return GestureDetector(
+      onTap: () => _resetFocusNode(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(locale.appName),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 32.0),
+          child: _BloodSugarStorageForm(),
+        ),
       ),
-      body: Container(),
     );
+  }
+
+  void _resetFocusNode(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
   }
 }
---
+++ lib/src/app/pages/widgets/_blood_sugar_storage_form.dart
@@ -0,0 +1,75 @@
+part of '../home_page.dart';
+
+class _BloodSugarStorageForm extends StatefulWidget {
+  @override
+  _BloodSugarStorageFormState createState() => _BloodSugarStorageFormState();
+}
+
+class _BloodSugarStorageFormState extends State<_BloodSugarStorageForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _sugarInBloodKey = GlobalKey<FormFieldState<String>>();
+
+  ThemeData get _theme => Theme.of(context);
+  DiaLocalizations get _locale => DiaLocalizations.of(context);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              _locale.enterSugarInBloodMeasure,
+              style: _theme.textTheme.subtitle2,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 70.0,
+              child: TextFormField(
+                key: _sugarInBloodKey,
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: _bloodSugarValidator,
+                decoration: InputDecoration(
+                  hintText: _locale.bloodSugarDataExample,
+                ),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            RaisedButton(
+              child: Text(_locale.sendNewMeasure),
+              onPressed: () {
+                if (_formKey.currentState.validate())
+                  print(_sugarInBloodKey.currentState.value);
+              },
+            ),
+            RaisedButton(
+              child: Text(_locale.showDiagnosis),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _bloodSugarValidator(value) {
+    String pattern = r'^\d+(\.\d{1,2})?$';
+    RegExp regExp = RegExp(pattern);
+    if (value.isEmpty) {
+      return _locale.errorInputData;
+    } else if (!regExp.hasMatch(value)) {
+      return _locale.errorExampleOfInput;
+    } else if (regExp.hasMatch(value)) {
+      double v = double.parse(value);
+      if (v < 0.0 || v > 70.0) {
+        return _locale.errorInvalidDataOfBloodSugar;
+      }
+    }
+    return null;
+  }
+}
